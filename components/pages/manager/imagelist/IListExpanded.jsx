47ae7174fee2a2243ba5f02135d5f24d40cb0046
ashraf-rtl <skabir@relisource.com>
2022-10-21 16:15:19 +0600

    expanding table staricon updated on imagelist page

--- components/pages/manager/imagelist/IListExpanded.jsx
+++ components/pages/manager/imagelist/IListExpanded.jsx
@@ -1,16 +1,6 @@
 import React, { useMemo, useState } from "react"
 import ExpandingTable from '@components/common/ExpandingTable/ExpandingTable'
-
-
-const StarIcon = ({ filled }) => (
-  <svg height="1rem" fill="none" viewBox="0 0 24 24" stroke={filled ? "#FF881B" : "#BDBDBD"} strokeWidth={2}>
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-    />
-  </svg>
-);
+import { StarOutline, StarFilled } from '@icons/index'
 
 const UpSvg = () => (
   <svg xmlns="http://www.w3.org/2000/svg" height="1rem" fill="none" viewBox="0 0 24 24" stroke="black" strokeWidth={2}>
@@ -40,7 +30,7 @@ const IListExpanded = () => {
         field: "accession_number",
         title: "Accession number",
         sortable: true,
-        //format the output data, expanding expandingHandler only avilable for expandable rows
+        //format the output data, expanding expandingHandler only available for expandable rows
         format: (value, _, expanding, expandingHandler) => (
           <span
             onClick={expandingHandler}
@@ -86,7 +76,7 @@ const IListExpanded = () => {
               justifyContent: "center",
             }}
           >
-            {value === true ? <StarIcon filled /> : <StarIcon />}
+            {value === true ? <StarFilled /> : <StarOutline />}
           </div>
         ),
       },
@@ -138,7 +128,7 @@ const IListExpanded = () => {
               justifyContent: "center",
             }}
           >
-            {value === true ? <StarIcon filled /> : <StarIcon />}
+            {value === true ? <StarFilled /> : <StarOutline />}
           </div>
         ),
       },
