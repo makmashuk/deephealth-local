a5d76faba90bef66459be5cba71a574186cfc90a
ashraf-rtl <skabir@relisource.com>
2022-10-25 16:37:06 +0600

    https and port updated

--- server.js
+++ server.js
@@ -6,17 +6,18 @@ const fs = require('fs')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const port = 8443
 
 const httpsOptions = {
-  key: fs.readFileSync('C:/cert/test_cert/localhost.key'),
-  cert: fs.readFileSync('C:/cert/test_cert/localhost.crt'),
+  key: fs.readFileSync('./cert/localhost.key'),
+  cert: fs.readFileSync('./cert/localhost.crt'),
 }
 
 app.prepare().then(() => {
   createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true)
     handle(req, res, parsedUrl)
-  }).listen(8443, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err
     console.log('> Server started on https://localhost:8443')
   })
