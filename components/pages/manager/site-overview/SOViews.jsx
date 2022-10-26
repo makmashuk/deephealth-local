e8fa3e943a2b8ef39996027ac704853154543b0b
Ashraf Kabir <skabir@relisource.com>
2022-10-26 21:12:04 +0600

    case sensitive import fixed

--- components/pages/manager/site-overview/SOViews.jsx
+++ components/pages/manager/site-overview/SOViews.jsx
@@ -2,7 +2,7 @@ import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 
-import { PieChart } from '@components/common/charts/PieChart'
+import { PieChart } from '@components/common/Charts/PieChart'
 
 export default function SOViews() {
   return (
